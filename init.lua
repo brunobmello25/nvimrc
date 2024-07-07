17350fa37821cbd4df8e01f0c11ea368b9fea935
Bruno Mello <bruno.barros.mello@gmail.com>
2024-07-06 21:31:52 -0300

    setup windows shell

--- init.lua
+++ init.lua
@@ -3,6 +3,16 @@ vim.g.maplocalleader = ' '
 
 vim.g.have_nerd_font = true
 
+if vim.fn.has 'win32' == 1 then
+  print 'setup windows'
+  vim.o.shellcmdflag =
+    '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+  vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+  vim.o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+  vim.o.shellquote = ''
+  vim.o.shellxquote = ''
+end
+
 require 'options'
 require 'keymaps'
 require 'lazy-bootstrap'
