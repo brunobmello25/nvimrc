d72bbd405f9c4141d7a2fc6632a16bdc59ed59e8
Bruno Mello <bruno.barros.mello@gmail.com>
2024-07-09 11:51:33 -0300

    dapui toggle shortcuts

--- lua/bruno/plugins/debug.lua
+++ lua/bruno/plugins/debug.lua
@@ -30,6 +30,12 @@ return {
     vim.keymap.set('n', '<F11>', dap.step_over, { desc = 'Debug: Step Over' })
     vim.keymap.set('n', '<F12>', dap.step_out, { desc = 'Debug: Step Out' })
     vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+    vim.keymap.set('n', '<leader>dr', function()
+      dapui.close()
+      dapui.open { reset = true }
+    end, { desc = 'DAP reset UI' })
+
+    vim.keymap.set('n', '<leader>du', '<cmd>lua require"dapui".toggle()<CR>', { desc = 'Debug: Toggle DAP-UI' })
     vim.keymap.set('n', '<leader>B', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
     end, { desc = 'Debug: Set Breakpoint' })
