f98f04bc8a6bcdf05ddd2c16bdd29b037a61f768
Bruno Mello <bruno.barros.mello@gmail.com>
2024-07-12 17:59:48 -0300

    change to gp.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -28,9 +28,13 @@ require('lazy').setup({
   require 'bruno/plugins/copilot',
   require 'bruno/plugins/zenmode',
   require 'bruno/plugins/folding',
+  require 'bruno/plugins/chatgpt',
   -- require 'bruno/plugins/neo-tree',
   -- require 'bruno/plugins/indent_line',
 }, {
+  change_detection = {
+    notify = false,
+  },
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
