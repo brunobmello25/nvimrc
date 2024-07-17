476c4978ed7a1a6920b6d19c4e52cab58bc3dd77
Bruno Mello <bruno.barros.mello@gmail.com>
2024-07-17 17:33:58 -0300

    refactor cmp config

---
+++ lua/plugins/config/cmp.lua
@@ -0,0 +1,48 @@
+-- See `:help cmp`
+local cmp = require 'cmp'
+local luasnip = require 'luasnip'
+luasnip.config.setup {}
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  completion = { completeopt = 'menu,menuone,noinsert' },
+
+  -- For an understanding of why these mappings were
+  -- chosen, you will need to read `:help ins-completion`
+  --
+  -- No, but seriously. Please read `:help ins-completion`, it is really good!
+  mapping = cmp.mapping.preset.insert {
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-y>'] = cmp.mapping.confirm { select = true },
+    ['<C-Space>'] = cmp.mapping.complete {},
+
+    ['<C-l>'] = cmp.mapping(function(fallback)
+      if luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
+    ['<C-h>'] = cmp.mapping(function(fallback)
+      if luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
+    -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+    --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'path' },
+  },
+}
