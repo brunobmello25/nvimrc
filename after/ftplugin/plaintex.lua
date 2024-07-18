bbd4b8a8841b33faef87f16aa8baed4879b21275
Bruno Mello <bruno.barros.mello@gmail.com>
2024-07-18 14:31:48 -0300

    change keymap description

--- after/ftplugin/plaintex.lua
+++ after/ftplugin/plaintex.lua
@@ -5,4 +5,4 @@ local function italicize()
   vim.cmd 'normal! a}'
 end
 
-vim.keymap.set('v', '<C-i>', italicize, { buffer = 0, desc = 'Insert a tab character' })
+vim.keymap.set('v', '<C-i>', italicize, { buffer = 0, desc = 'Italicize selected text' })
