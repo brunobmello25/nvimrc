78f4282506125fef8f0817e91e73f9499a1acd9d
Bruno Mello <bruno.barros.mello@gmail.com>
2024-07-19 10:46:19 -0300

    using supermaven instead of copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,27 +1,42 @@
 return {
   {
+    'supermaven-inc/supermaven-nvim',
+    config = function()
+      require('supermaven-nvim').setup {
+        keymaps = {
+          accept_suggestion = '<C-u>',
+        },
+        -- ignore_filetypes = { cpp = true },
+        log_level = 'info', -- set to "off" to disable logging completely
+        disable_inline_completion = false, -- disables inline completion for use with cmp
+        disable_keymaps = false, -- disables built in keymaps for more manual control
+      }
+    end,
+  },
+
+  {
     'zbirenbaum/copilot.lua',
     cmd = 'Copilot',
     event = 'InsertEnter',
     config = function()
       local copilot = require 'copilot'
-      local suggestion = require 'copilot.suggestion'
-      local autopairs = require 'nvim-autopairs'
+      -- local suggestion = require 'copilot.suggestion'
+      -- local autopairs = require 'nvim-autopairs'
 
       copilot.setup {
-        suggestion = {
-          auto_trigger = true,
-        },
-        filetypes = {
-          ['*'] = true,
-        },
+        -- suggestion = {
+        --   auto_trigger = true,
+        -- },
+        -- filetypes = {
+        --   ['*'] = true,
+        -- },
       }
 
-      vim.keymap.set('i', '<C-u>', function()
-        autopairs.disable()
-        suggestion.accept()
-        autopairs.enable()
-      end, { desc = 'Accept Copilot suggestion' })
+      -- vim.keymap.set('i', '<C-u>', function()
+      --   autopairs.disable()
+      --   suggestion.accept()
+      --   autopairs.enable()
+      -- end, { desc = 'Accept Copilot suggestion' })
     end,
   },
 
