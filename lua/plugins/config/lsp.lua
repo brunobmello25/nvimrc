998a97cd08939fd08c0b170cec0f0b5e07c9d348
Bruno Mello <bruno.barros.mello@gmail.com>
2024-07-22 22:59:19 -0300

    setup gdscript lsp

--- lua/plugins/config/lsp.lua
+++ lua/plugins/config/lsp.lua
@@ -1,34 +1,38 @@
+local on_attach = function(client, bufnr)
+  local map = function(keys, func, desc)
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = 'LSP: ' .. desc })
+  end
+
+  map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+  map('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
+  map('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+  map('gT', vim.lsp.buf.type_definition, 'Type [D]efinition')
+  map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  map('<leader>.', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  map('K', vim.lsp.buf.hover, 'Hover Documentation')
+  map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+  if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+    map('<leader>th', function()
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled {})
+    end, '[T]oggle Inlay [H]ints')
+  end
+
+  vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = 'rounded',
+  })
+  vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' })
+  vim.diagnostic.config {
+    float = { border = 'rounded' },
+  }
+end
+
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
   callback = function(event)
-    local map = function(keys, func, desc)
-      vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-    end
-
-    map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-    map('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
-    map('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-    map('gT', vim.lsp.buf.type_definition, 'Type [D]efinition')
-    map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    map('<leader>.', vim.lsp.buf.code_action, '[C]ode [A]ction')
-    map('K', vim.lsp.buf.hover, 'Hover Documentation')
-    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
     local client = vim.lsp.get_client_by_id(event.data.client_id)
 
-    if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-      map('<leader>th', function()
-        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled {})
-      end, '[T]oggle Inlay [H]ints')
-    end
-
-    vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-      border = 'rounded',
-    })
-    vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' })
-    vim.diagnostic.config {
-      float = { border = 'rounded' },
-    }
+    on_attach(client, event.buf)
   end,
 })
 
@@ -81,4 +85,11 @@ require('mason-lspconfig').setup {
       require('lspconfig')[server_name].setup(server)
     end,
   },
+}
+
+require('lspconfig').gdscript.setup {
+  cmd = { 'ncat', 'localhost', 6005 },
+  on_attach = function(client, bufnr)
+    on_attach(client, bufnr)
+  end,
 }
