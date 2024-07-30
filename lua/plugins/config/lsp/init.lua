20f3d61667c632716984e32e749fe368950276ea
Bruno Mello <bruno.barros.mello@gmail.com>
2024-07-30 14:58:33 -0300

    split lsp on attach function into separate file

--- lua/plugins/config/lsp/init.lua
+++ lua/plugins/config/lsp/init.lua
@@ -6,33 +6,12 @@ vim.diagnostic.config {
   float = { border = 'rounded' },
 }
 
-local on_attach = function(client, bufnr)
-  local map = function(keys, func, desc)
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = 'LSP: ' .. desc })
-  end
-
-  map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  map('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
-  map('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  map('gT', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  map('<leader>.', vim.lsp.buf.code_action, '[C]ode [A]ction')
-  map('K', vim.lsp.buf.hover, 'Hover Documentation')
-  map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-  if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-    map('<leader>th', function()
-      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled {})
-    end, '[T]oggle Inlay [H]ints')
-  end
-end
-
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
   callback = function(event)
     local client = vim.lsp.get_client_by_id(event.data.client_id)
 
-    on_attach(client, event.buf)
+    require('plugins.config.lsp.on_attach').on_attach(client, event.buf)
   end,
 })
 
