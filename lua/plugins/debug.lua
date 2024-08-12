273f676db6ad966b431ceb03e5a4e562e4731972
Bruno Mello <bruno.barros.mello@gmail.com>
2024-08-12 14:45:17 -0300

    adjust dap virtual text position

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -19,7 +19,9 @@ return {
     'theHamsta/nvim-dap-virtual-text',
     dependencies = { 'mfussenegger/nvim-dap', 'nvim-treesitter/nvim-treesitter' },
     config = function()
-      require('nvim-dap-virtual-text').setup {}
+      require('nvim-dap-virtual-text').setup {
+        virt_text_pos = 'eol',
+      }
     end,
   },
 }
