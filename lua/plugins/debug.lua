8d0efd1e17f72114f4ccb4dad870b134c24cddd4
Bruno Mello <bruno.barros.mello@gmail.com>
2024-08-12 16:31:37 -0300

    refactor

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -18,10 +18,8 @@ return {
   {
     'theHamsta/nvim-dap-virtual-text',
     dependencies = { 'mfussenegger/nvim-dap', 'nvim-treesitter/nvim-treesitter' },
-    config = function()
-      require('nvim-dap-virtual-text').setup {
-        virt_text_pos = 'eol',
-      }
-    end,
+    opts = {
+      virt_text_pos = 'eol',
+    },
   },
 }
