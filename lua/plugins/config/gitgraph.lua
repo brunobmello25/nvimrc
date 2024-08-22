5e46670650725bf3cfe86d1f07c67dc5c9acecd4
Bruno Mello <bruno.barros.mello@gmail.com>
2024-08-22 14:19:04 -0300

    remove vim.notify from gitgraph hooks

--- lua/plugins/config/gitgraph.lua
+++ lua/plugins/config/gitgraph.lua
@@ -10,12 +10,10 @@ require('gitgraph').setup {
   hooks = {
 
     on_select_commit = function(commit)
-      vim.notify('DiffviewOpen ' .. commit.hash .. '^!')
       vim.cmd(':DiffviewOpen ' .. commit.hash .. '^!')
     end,
     -- Check diff from commit a -> commit b
     on_select_range_commit = function(from, to)
-      vim.notify('DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
       vim.cmd(':DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
     end,
   },
