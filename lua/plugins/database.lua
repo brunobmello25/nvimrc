4f71cb2275a2f1aae13df6dabd2ee59fc8012ceb
Bruno Mello <bruno.barros.mello@gmail.com>
2024-09-04 11:32:20 -0300

    fix: vim dad bod completion

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -2,6 +2,16 @@ return {
   'tpope/vim-dadbod',
   dependencies = {
     'kristijanhusak/vim-dadbod-ui',
-    'kristijanhusak/vim-dadbod-completion',
+    {
+      'kristijanhusak/vim-dadbod-completion',
+      config = function()
+        require('cmp').setup.filetype({ 'sql', 'mysql', 'plsql' }, {
+          sources = {
+            { name = 'vim-dadbod-completion' },
+            { name = 'buffer' },
+          },
+        })
+      end,
+    },
   },
 }
