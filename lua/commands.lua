fc9cf67a4a91e51c089b1e3ad7f2ccfabfee6528
Bruno Mello <bruno.barros.mello@gmail.com>
2024-09-10 12:10:00 -0300

    add uuid command

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,1 +1,7 @@
 vim.cmd "command! CopyRelPath call setreg('+', expand('%:p:.'))"
+
+vim.api.nvim_create_user_command('UUID', function()
+  local output = vim.fn.system 'uuidgen'
+
+  vim.fn.setreg('"', vim.trim(output))
+end, { desc = 'Generate a new uuid' })
