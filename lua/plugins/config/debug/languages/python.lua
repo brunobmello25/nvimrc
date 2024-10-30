32218506b1a6d52775c76f52dfa6aeb4015b7225
Bruno Mello <bruno.barros.mello@gmail.com>
2024-10-30 14:00:46 -0300

    setup python debug with windows

--- lua/plugins/config/debug/languages/python.lua
+++ lua/plugins/config/debug/languages/python.lua
@@ -1,1 +1,5 @@
-require('dap-python').setup '~/.local/share/nvim/mason/packages/debugpy/venv/bin/python'
+if vim.fn.has 'win32' == 1 then
+  require('dap-python').setup(vim.fn.stdpath 'data' .. '\\mason\\packages\\debugpy\\venv\\Scripts\\python.exe')
+else
+  require('dap-python').setup '~/.local/share/nvim/mason/packages/debugpy/venv/bin/python'
+end
