8c2b5247aa8480e63f8b015f877e7751d37bedc5
Bruno Mello <bruno.barros.mello@gmail.com>
2024-12-27 10:07:32 -0300

    switch to luasnip temporarily to fix the highlight bug

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -2,7 +2,7 @@
 return {
   {
     'saghen/blink.cmp',
-    dependencies = 'rafamadriz/friendly-snippets',
+    dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
 
     version = 'v0.*',
     ---@module 'blink.cmp'
@@ -19,6 +19,22 @@ return {
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono',
+      },
+
+      -- TODO: should switch back to friend-snippets when the highlight bug is fixed
+      snippets = {
+        expand = function(snippet)
+          require('luasnip').lsp_expand(snippet)
+        end,
+        active = function(filter)
+          if filter and filter.direction then
+            return require('luasnip').jumpable(filter.direction)
+          end
+          return require('luasnip').in_snippet()
+        end,
+        jump = function(direction)
+          require('luasnip').jump(direction)
+        end,
       },
 
       sources = {
