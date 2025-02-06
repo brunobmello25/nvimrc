fe3dcb4e9996792f77e08df802191c80d0744eba
Bruno Mello <bruno.barros.mello@gmail.com>
2025-02-06 15:01:59 -0300

    wip snacks.pick plugin to replace telescope

--- lua/plugins/pick.lua
+++ lua/plugins/pick.lua
@@ -9,11 +9,69 @@ return {
         enabled = true,
       },
     },
+    dependencies = {
+      {
+        'folke/todo-comments.nvim',
+        config = true,
+      },
+    },
+    -- TODO:
+    -- list yanked items
+    -- customize grep with ctrl+space
     keys = {
       {
         '<leader>ff',
         function()
-          require('snacks').picker.files()
+          Snacks.picker.files()
+        end,
+      },
+      {
+        '<leader>fF',
+        function()
+          Snacks.picker.files { hidden = true }
+        end,
+      },
+      {
+        '<leader>fb',
+        function()
+          Snacks.picker.buffers()
+        end,
+      },
+      {
+        '<leader>fd',
+        function()
+          Snacks.picker.diagnostics()
+        end,
+      },
+      {
+        '<leader>fg',
+        function()
+          Snacks.picker.grep()
+        end,
+      },
+      {
+        '<leader>ft',
+        function()
+          Snacks.picker.todo_comments()
+        end,
+        desc = 'Todo',
+      },
+      {
+        '<leader>fh',
+        function()
+          Snacks.picker.help()
+        end,
+      },
+      {
+        '<leader>fk',
+        function()
+          Snacks.picker.keymaps()
+        end,
+      },
+      {
+        '<leader>fr',
+        function()
+          Snacks.picker.resume()
         end,
       },
     },
