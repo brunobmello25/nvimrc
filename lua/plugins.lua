d532c8a4f0093300e9bcbf03a79c0cf0464f1686
Bruno Mello <bruno.barros.mello@gmail.com>
2025-03-07 18:19:42 -0300

    add nvim project marks

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,6 +5,8 @@ require('lazy').setup({
 
   { 'kevinhwang91/nvim-bqf', dependencies = { 'nvim-treesitter/nvim-treesitter' } }, -- better quickfix window experience (preview, filter items etc)
 
+  { 'BartSte/nvim-project-marks', lazy = false, opts = {} },
+
   require 'plugins.csv',
   require 'plugins.comment', -- "gc" to comment visual regions/lines
   require 'plugins.flash', -- code navigation with "s" key
