5d130e3c17bb05ddbbd3e09cc63911301c945bef
Bruno Mello <bruno.barros.mello@gmail.com>
2025-03-18 18:06:51 -0300

    switch to moonfly

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -7,18 +7,24 @@ local themes = {
     source = 'ficcdaf/ashen.nvim',
     cmd_name = 'ashen',
   },
+  moonfly = {
+    cmd_name = 'moonfly',
+    source = 'bluz71/vim-moonfly-colors',
+  },
 }
-local active_theme = 'cyberdream'
+local active_theme = 'moonfly'
 
 local active = themes[active_theme]
 
+local params = {
+  active.source,
+  lazy = false,
+  priority = 1000,
+  init = function()
+    vim.cmd(string.format('colorscheme %s', themes[active_theme].cmd_name))
+  end,
+}
+
 return {
-  {
-    active.source,
-    lazy = false,
-    priority = 1000,
-    init = function()
-      vim.cmd(string.format('colorscheme %s', themes[active_theme].cmd_name))
-    end,
-  },
+  params,
 }
