d77e4b6251e7a5774d8de97f75924e5aad2fb1df
Bruno Mello <bruno.barros.mello@gmail.com>
2025-04-16 10:21:04 -0300

    adjust nvim lint golang params

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -4,12 +4,17 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
+
+      local goci = lint.linters.golangcilint
+      goci.args = { 'run', '--output.json.path=stdout', '--show-stats=false', '--issues-exit-code', '0' }
+
       lint.linters_by_ft = lint.linters_by_ft or {}
       lint.linters_by_ft['typescript'] = { 'eslint' }
       lint.linters_by_ft['javascript'] = { 'eslint' }
       lint.linters_by_ft['typescriptreact'] = { 'eslint' }
       lint.linters_by_ft['javascriptreact'] = { 'eslint' }
       lint.linters_by_ft['yaml'] = { 'yamllint' }
+      lint.linters_by_ft['go'] = { 'golangcilint' }
 
       local disabled = { 'terraform', 'ruby', 'rst', 'markdown', 'janet', 'inko', 'dockerfile', 'clojure', 'text', 'markdown' }
       for _, linter in ipairs(disabled) do
