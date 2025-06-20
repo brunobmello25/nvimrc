fc000d273bf8f6cf76389308d64ce93f77c4d0c7
Bruno Mello <bruno.barros.mello@gmail.com>
2025-06-20 12:22:36 -0300

    setup plantuml autobuild

--- lua/commands/setupautocommand.lua
+++ lua/commands/setupautocommand.lua
@@ -2,6 +2,7 @@
 --- saving a specific buffer. For example, for running `plantuml something.puml` everytime
 --- we save `something.puml`, run `:SetupCommandOnSave` and then pass in
 --- the command `plantuml something.puml`
+
 vim.api.nvim_create_user_command('SetupCommandOnSave', function()
   local cmd = vim.fn.input 'Command to run on save: '
 
@@ -17,4 +18,25 @@ vim.api.nvim_create_user_command('SetupCommandOnSave', function()
     end,
   })
   print('On‑save hook set: ' .. cmd)
+end, {})
+
+vim.api.nvim_create_user_command('PlantUMLAutoBuild', function()
+  local ext = vim.fn.expand '%:e'
+
+  if ext ~= 'puml' and ext ~= 'plantuml' then
+    local msg = 'This command only works for .puml or .plantuml files.'
+    vim.notify(msg, vim.log.levels.WARN)
+    return
+  end
+
+  local path = vim.fn.expand '%:p'
+
+  vim.api.nvim_create_autocmd('BufWritePost', {
+    group = vim.api.nvim_create_augroup('PlantUMLAutoBuildGroup', { clear = true }),
+    buffer = vim.api.nvim_get_current_buf(),
+    callback = function()
+      local cmd = 'plantuml ' .. path
+      vim.fn.jobstart(cmd, { stdout_buffered = true })
+    end,
+  })
 end, {})
