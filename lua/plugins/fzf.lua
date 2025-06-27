c41e557cae9e0075d209ddd61b9cd86cbcb8ffe7
Bruno Mello <bruno.barros.mello@gmail.com>
2025-06-27 16:27:31 -0300

    fix todo comment on fzf lua

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,7 +1,10 @@
 return {
   {
     'ibhagwan/fzf-lua',
-    dependencies = { 'nvim-tree/nvim-web-devicons', 'folke/todo-comments.nvim' },
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+      { 'folke/todo-comments.nvim', opts = {} },
+    },
     opts = {
       'telescope',
       winopts = {
@@ -16,10 +19,7 @@ return {
       vim.keymap.set('n', '<leader>fb', ':FzfLua buffers<CR>', { desc = 'FzfLua: Buffers' })
       vim.keymap.set('n', '<leader>fd', ':FzfLua diagnostics_workspace<CR>', { desc = 'FzfLua: Diagnostics' })
       vim.keymap.set('n', '<leader>fg', ':FzfLua live_grep<CR>', { desc = 'FzfLua: Live Grep' })
-      vim.keymap.set('n', '<leader>ft', ':FzfLua todo_comments<CR>', { desc = 'FzfLua: Todo Comments' })
-      vim.keymap.set('n', '<leader>ft', function()
-        require('todo-comments.fzf').todo {}
-      end, { desc = 'FzfLua: Todo Comments' })
+      vim.keymap.set('n', '<leader>ft', ':TodoFzfLua<CR>', { desc = 'FzfLua: Todo Comments' })
       vim.keymap.set('n', '<leader>fh', ':FzfLua help_tags<CR>', { desc = 'FzfLua: Help Tags' })
       vim.keymap.set('n', '<leader>fk', ':FzfLua keymaps<CR>', { desc = 'FzfLua: Keymaps' })
       vim.keymap.set('n', '<leader>fr', ':FzfLua resume<CR>', { desc = 'FzfLua: Resume' })
