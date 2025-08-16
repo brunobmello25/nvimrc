845ec080834f1c68b7a2946cff6e429d9e3b62a5
Bruno Mello <bruno.barros.mello@gmail.com>
2025-08-16 11:44:33 -0300

    setup zig debugging

--- lua/plugins/config/debug/adapters/codelldb.lua
+++ lua/plugins/config/debug/adapters/codelldb.lua
@@ -1,15 +1,25 @@
 local dap = require 'dap'
 
-local mason_path = vim.fn.stdpath 'data' .. '/mason'
-local codelldb_path = mason_path .. '/packages/codelldb'
-local liblldb_path = codelldb_path .. '/extension/lldb/lib/liblldb.so'
+-- local mason_path = vim.fn.stdpath 'data' .. '/mason'
+
+-- local codelldb_path = mason_path .. '/packages/codelldb'
+-- local liblldb_path = codelldb_path .. '/extension/lldb/lib/liblldb.so'
 -- local codelldb_path = extension_path .. 'adapter/codelldb'
 -- local liblldb_path = extension_path .. 'lldb/lib/liblldb.so' -- adjust if needed
+-- dap.adapters.codelldb = {
+--   type = 'executable',
+--   executable = {
+--     command = codelldb_path .. '/codelldb',
+--     args = { '--liblldb-path', liblldb_path },
+--   },
+-- }
 
+-- configure codelldb adapter
 dap.adapters.codelldb = {
-  type = 'executable',
+  type = 'server',
+  port = '${port}',
   executable = {
-    command = codelldb_path .. '/codelldb',
-    args = { '--liblldb-path', liblldb_path },
+    command = 'codelldb',
+    args = { '--port', '${port}' },
   },
 }
