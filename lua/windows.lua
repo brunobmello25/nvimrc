12c13b8a25ebd6e421850c9a6d192a80ba3657db
Bruno Mello <bruno.barros.mello@gmail.com>
2025-08-24 15:56:17 -0300

    windows config

--- lua/windows.lua
+++ lua/windows.lua
@@ -128,6 +128,48 @@ require('lazy').setup({
       vim.keymap.set('n', 'gj', '<cmd>diffget //3<CR>', { desc = 'Get right side of diff' })
     end,
   },
+
+  -- Formatting
+  {
+    'stevearc/conform.nvim',
+    enabled = function()
+      local root = vim.fn.getcwd()
+      local conform_ignore = vim.fn.glob(root .. '/.conform-ignore', false, true)
+      if #conform_ignore > 0 then
+        return false
+      end
+      return true
+    end,
+    opts = {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        yaml = { 'prettier' },
+        html = { 'prettier' },
+        htmldjango = { 'prettier' },
+        css = { 'prettier' },
+        json = { 'jq' },
+        python = { 'autopep8' },
+        gdscript = { 'gdformat' },
+        markdown = { 'markdownlint' },
+        sql = { 'sql_formatter' },
+        c = { 'clang-format' },
+        cpp = { 'clang-format' },
+      },
+
+      format_on_save = {
+        lsp_fallback = true,
+      },
+    },
+    init = function()
+      vim.api.nvim_create_user_command('ConformFormat', function()
+        require('conform').format()
+      end, {})
+    end,
+  },
 }, {
   change_detection = {
     notify = false,
