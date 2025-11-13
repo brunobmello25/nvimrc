5295624e5823cd08e2a122300a0758bfe9ac212c
Bruno Mello <bruno.barros.mello@gmail.com>
2025-11-13 17:13:06 -0300

    switch to root markers

--- lua/plugins/config/lsp/languages/gdscript.lua
+++ lua/plugins/config/lsp/languages/gdscript.lua
@@ -1,13 +1,6 @@
-local function root_pattern(...)
-  local patterns = { ... }
-  return function(path)
-    return vim.fs.find(patterns, { path = path, upward = true })[1]
-  end
-end
-
 vim.lsp.config.gdscript = {
   cmd = { 'ncat', 'localhost', os.getenv 'GDScript_Port' or '6005' },
-  root_dir = root_pattern('project.godot', '.git'),
+  root_markers = { 'project.godot', '.git' },
   single_file_support = false,
   filetypes = { 'gdscript' },
 }
--- lua/plugins/config/lsp/languages/swiprolog.lua
+++ lua/plugins/config/lsp/languages/swiprolog.lua
@@ -1,14 +1,7 @@
-local function root_pattern(...)
-  local patterns = { ... }
-  return function(path)
-    return vim.fs.find(patterns, { path = path, upward = true })[1]
-  end
-end
-
 vim.lsp.config.prolog_ls = {
   cmd = { 'swipl', '-g', 'use_module(library(lsp_server)).', '-g', 'lsp_server:main', '-t', 'halt', '--', 'stdio' },
   filetypes = { 'prolog' },
-  root_dir = root_pattern('.git', '*.pl'),
+  root_markers = { '.git', '*.pl' },
   settings = {
     prolog_ls = {
       -- Custom settings for prolog_ls can go here
