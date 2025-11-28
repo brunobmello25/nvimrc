24c23191717bb52c7526b05e71e3df4611b10277
Bruno Mello <bruno.barros.mello@gmail.com>
2025-11-28 16:20:29 -0300

    add no neck pain

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -21,6 +21,25 @@ require('lazy').setup({
   { 'chentoast/marks.nvim', event = 'VeryLazy', opts = {} },
 
   { 'HawkinsT/pathfinder.nvim' },
+  {
+    'shortcuts/no-neck-pain.nvim',
+    version = '*',
+    opts = {
+      buffers = {
+        scratchPad = {
+          -- set to `false` to
+          -- disable auto-saving
+          enabled = true,
+          -- set to `nil` to default
+          -- to current working directory
+          location = '~/Documents/',
+        },
+        bo = {
+          filetype = 'md',
+        },
+      },
+    },
+  },
 
   require 'plugins.imgclip',
   require 'plugins.telescope',
