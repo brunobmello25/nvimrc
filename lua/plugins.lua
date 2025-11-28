5d6579e5b613d9da6da24385c5f640550c62d61a
Bruno Mello <bruno.barros.mello@gmail.com>
2025-11-28 19:24:54 -0300

    extract neck pain plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -21,25 +21,7 @@ require('lazy').setup({
   { 'chentoast/marks.nvim', event = 'VeryLazy', opts = {} },
 
   { 'HawkinsT/pathfinder.nvim' },
-  {
-    'shortcuts/no-neck-pain.nvim',
-    version = '*',
-    opts = {
-      buffers = {
-        scratchPad = {
-          -- set to `false` to
-          -- disable auto-saving
-          enabled = true,
-          -- set to `nil` to default
-          -- to current working directory
-          location = '~/Documents/',
-        },
-        bo = {
-          filetype = 'md',
-        },
-      },
-    },
-  },
+  require 'plugins.no-neck-pain',
 
   require 'plugins.imgclip',
   require 'plugins.telescope',
---
+++ lua/plugins/no-neck-pain.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    'shortcuts/no-neck-pain.nvim',
+    version = '*',
+    keys = {
+      { '<leader>nn', '<cmd>NoNeckPain<cr>', desc = 'Toggle No Neck Pain' },
+    },
+    opts = {
+      buffers = {
+        scratchPad = {
+          -- set to `false` to
+          -- disable auto-saving
+          enabled = true,
+          -- set to `nil` to default
+          -- to current working directory
+          location = '~/Documents/',
+        },
+        bo = {
+          filetype = 'md',
+        },
+      },
+    },
+  },
+}
