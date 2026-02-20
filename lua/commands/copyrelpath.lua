1fa112bb947ed119412275e44bd1f7f74bf575b4
Bruno Mello <bruno.barros.mello@gmail.com>
2026-02-19 21:28:28 -0300

    support visual selection for copy rel path

--- lua/commands/copyrelpath.lua
+++ lua/commands/copyrelpath.lua
@@ -1,1 +1,7 @@
-vim.cmd "command! CopyRelPath call setreg('+', expand('%:p:.'))"
+vim.api.nvim_create_user_command("CopyRelPath", function(opts)
+  local path = vim.fn.expand("%:p:.")
+  if opts.range > 0 then
+    path = path .. ":" .. opts.line1 .. "," .. opts.line2
+  end
+  vim.fn.setreg("+", path)
+end, { range = true })
