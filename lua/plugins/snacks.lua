68288c692d38448c24c28579f0ae66fcde0eb85f
Bruno Mello <bruno.barros.mello@gmail.com>
2026-03-05 16:38:07 -0300

    disable snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,17 +1,17 @@
 return {
-  {
-    'folke/snacks.nvim',
-    priority = 1000,
-    lazy = false,
-    ---@type snacks.Config
-    opts = {
-      image = {
-        backend = 'kitty',
-        integrations = {
-          markdown = { enabled = true },
-        },
-      },
-      terminal = {},
-    },
-  },
+  -- {
+  --   'folke/snacks.nvim',
+  --   priority = 1000,
+  --   lazy = false,
+  --   ---@type snacks.Config
+  --   opts = {
+  --     image = {
+  --       backend = 'kitty',
+  --       integrations = {
+  --         markdown = { enabled = true },
+  --       },
+  --     },
+  --     terminal = {},
+  --   },
+  -- },
 }
