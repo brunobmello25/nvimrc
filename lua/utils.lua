33247b47dd528af7b7c0c3f714cc24adadf1b6d6
Bruno Mello <bruno.barros.mello@gmail.com>
2026-05-02 13:20:56 -0300

    adjust lockfile and remove unecessary print

--- lua/utils.lua
+++ lua/utils.lua
@@ -6,12 +6,10 @@ end
 local function load_project_config()
   local cwd = vim.fn.getcwd()
   local project_config_path = cwd .. '/.nvimrc/init.lua'
-  
+
   if vim.fn.filereadable(project_config_path) == 1 then
     local success, err = pcall(dofile, project_config_path)
-    if success then
-      vim.notify('Loaded project config: ' .. project_config_path, vim.log.levels.INFO)
-    else
+    if not success then
       vim.notify('Error loading project config: ' .. err, vim.log.levels.ERROR)
     end
   end
