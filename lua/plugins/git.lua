0b35503112f2cdd780f945971fbc9514e71aa364
Bruno Mello <bruno.barros.mello@gmail.com>
2026-05-06 16:48:47 -0300

    remove git signs

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,10 +1,5 @@
 return {
   { 'tpope/vim-fugitive' },
-  {
-    'lewis6991/gitsigns.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {},
-  },
 
   { 'akinsho/git-conflict.nvim', version = '*', config = true },
 
