918232de501e2ad19242b2af2747076b319b891d
Bruno Mello <bruno.barros.mello@gmail.com>
2026-05-19 12:41:37 -0300

    add odin debug test

---
+++ after/ftplugin/odin.lua
@@ -0,0 +1,31 @@
+vim.keymap.set('n', '<leader>dt', function()
+  local dap = require 'dap'
+  local dir = vim.fn.expand '%:p:h'
+  local test_binary = dir .. '/.odin_dap_test'
+
+  os.remove(test_binary)
+
+  vim.fn.system {
+    'odin',
+    'test',
+    dir,
+    '-debug',
+    '-keep-executable',
+    '-out:' .. test_binary,
+  }
+
+  if not vim.fn.filereadable(test_binary) then
+    vim.notify('odin test compilation failed', vim.log.levels.ERROR)
+    return
+  end
+
+  dap.run {
+    type = 'codelldb',
+    request = 'launch',
+    name = 'Debug Odin Tests',
+    program = test_binary,
+    cwd = dir,
+    stopOnEntry = false,
+    args = {},
+  }
+end, { desc = 'Debug odin tests' })
