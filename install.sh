820c508a4ac389f8cce35e9560e04fd5d84cc367
Bruno Mello <bruno.barros.mello@gmail.com>
2026-05-26 11:31:56 -0300

    update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-[ -d ~/.config/nvim ] && echo "Moving old config to ~/nvim-bck" && mv ~/.config/nvim ~/nvim-bck
+[ -d ~/.config/nvim2 ] && echo "Moving old config to ~/nvim-bck" && mv ~/.config/nvim2 ~/nvim-bck
 
-echo "Linking to ~/.config/nvim"
-ln -s $(pwd) ~/.config/nvim
+echo "Linking to ~/.config/nvim2"
+ln -s $(pwd) ~/.config/nvim2
 
